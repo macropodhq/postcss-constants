5d6f780c54e36be906187821fc390341cdce89c5
James Coleman <james.coleman@bugherd.com>
2015-07-07 12:17:17 +1000

    Clean up; with no more warnings

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var postcss = require('postcss');
 var fs = require('fs');
 
-module.exports = postcss.plugin('postcss-local-vars', function (opts) {
+module.exports = postcss.plugin('postcss-local-vars', function () {
     var importRegex = /((import)(\s+)(local)(\.))((?:[A-z]+))( )(from)( )(".*?"|'.*?')/g;
     var nameRegex = /.*?((local)(\.))((?:[A-z]+))/g;
 
     var readFile = function(file) {
         return fs.readFileSync(file, 'utf8');
-    }
+    };
 
     var requiresAction = function(context) {
-      return context.indexOf('import local.') !== -1;
+        return context.indexOf('import local.') !== -1;
     };
 
     var getPath = function(context) {
@@ -18,18 +18,18 @@ module.exports = postcss.plugin('postcss-local-vars', function (opts) {
         return pathMatches[pathMatches.length - 1].replace(/'|"/g, '');
     };
 
-    var getName = function(context, node) {
+    var getName = function(context) {
         var nameMatches = nameRegex.exec(context);
         return nameMatches[nameMatches.length - 1];
-    }
+    };
 
-    var strip = function(context, node) {
+    var strip = function(context) {
         if (!requiresAction(context)) {
-          return context;
+            return context;
         }
 
         var variablesFile = readFile(getPath(context));
-        var variablesRegex = new RegExp('(local\.' + getName(context, node) + ')(:).*?(.*?)(;)', 'g');
+        var variablesRegex = new RegExp('(local\.' + getName(context) + ')(:).*?(.*?)(;)', 'g');
         var variables = variablesRegex.exec(variablesFile);
         var value = variables[variables.length - 2].trim();
 
@@ -39,11 +39,11 @@ module.exports = postcss.plugin('postcss-local-vars', function (opts) {
     return function (css) {
         css.eachInside(function (node) {
             if (node.type === 'decl') {
-                node.value = strip(node.value, node);
+                node.value = strip(node.value);
             }
 
             if (node.type === 'atrule') {
-              node.params = strip(node.params, node);
+                node.params = strip(node.params);
             }
         });
     };
