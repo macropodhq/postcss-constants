a613ed62ff91eb7aa28c6773723977a3b6eaee80
James Coleman <james.coleman@bugherd.com>
2015-08-04 12:18:06 +1000

    Fix path, so it works from anywhere

--- test/test.js
+++ test/test.js
@@ -19,42 +19,42 @@ describe('postcss-local-constants', function () {
     });
 
     it('replaces constants in values', function (done) {
-        test('~colors: "test/constants.js"; a{color: primary from ~colors;}', 'a{color: #8EE7D3;}', { }, done);
+        test('~colors: "./test/constants.js"; a{color: primary from ~colors;}', 'a{color: #8EE7D3;}', { }, done);
     });
 
     it('replaces constants in values without semi-colons', function (done) {
-        test('~colors: "test/constants.js"; a{background: blue; color: primary from ~colors}', 'a{background: blue; color: #8EE7D3}', { }, done);
+        test('~colors: "./test/constants.js"; a{background: blue; color: primary from ~colors}', 'a{background: blue; color: #8EE7D3}', { }, done);
     });
 
     it('replaces constants at start of value', function (done) {
-        test('~borders: "test/constants.js"; a{border: weight from ~borders solid #8EE7D3;}', 'a{border: 2px solid #8EE7D3;}', { }, done);
+        test('~borders: "./test/constants.js"; a{border: weight from ~borders solid #8EE7D3;}', 'a{border: 2px solid #8EE7D3;}', { }, done);
     });
 
     it('replaces constants in middle of value', function (done) {
-        test('~borders: "test/constants.js"; a{border: 2px style from ~borders #8EE7D3;}', 'a{border: 2px solid #8EE7D3;}', { }, done);
+        test('~borders: "./test/constants.js"; a{border: 2px style from ~borders #8EE7D3;}', 'a{border: 2px solid #8EE7D3;}', { }, done);
     });
 
     it('replaces constants at end of value', function (done) {
-        test('~colors: "test/constants.js"; a{border: 2px solid primary from ~colors;}', 'a{border: 2px solid #8EE7D3;}', { }, done);
+        test('~colors: "./test/constants.js"; a{border: 2px solid primary from ~colors;}', 'a{border: 2px solid #8EE7D3;}', { }, done);
     });
 
     it('replaces constants in @ rules', function (done) {
-        test('~queries: "test/constants.js"; @media (max-width: maxWidth from ~queries) {color: red;}', '@media (max-width: 200px) {color: red;}', { }, done);
+        test('~queries: "./test/constants.js"; @media (max-width: maxWidth from ~queries) {color: red;}', '@media (max-width: 200px) {color: red;}', { }, done);
     });
 
     it('multiple sources', function (done) {
-        test('~colors: "test/constants.js"; ~borders: "test/constants.js"; a{color: primary from ~colors; border: 2px style from ~borders black}', 'a{color: #8EE7D3; border: 2px solid black}', { }, done);
+        test('~colors: "./test/constants.js"; ~borders: "./test/constants.js"; a{color: primary from ~colors; border: 2px style from ~borders black}', 'a{color: #8EE7D3; border: 2px solid black}', { }, done);
     });
 
     it('multiple constants in a single value', function (done) {
-        test('~borders: "test/constants.js"; a{border: weight from ~borders style from ~borders black}', 'a{border: 2px solid black}', { }, done);
+        test('~borders: "./test/constants.js"; a{border: weight from ~borders style from ~borders black}', 'a{border: 2px solid black}', { }, done);
     });
 
     it('overrides default constants', function (done) {
-        test('~colors: "test/constants.js"; a{color: primary from ~colors;}', 'a{color: #8EE7D3;}', { defaults: {colors: {primary: 'red'} } }, done);
+        test('~colors: "./test/constants.js"; a{color: primary from ~colors;}', 'a{color: #8EE7D3;}', { defaults: {colors: {primary: 'red'} } }, done);
     });
 
     it('default constants', function (done) {
-        test('~colors: "test/constants.js"; a{color: secondary from ~colors;}', 'a{color: red;}', { defaults: {colors: {secondary: 'red'} } }, done);
+        test('~colors: "./test/constants.js"; a{color: secondary from ~colors;}', 'a{color: red;}', { defaults: {colors: {secondary: 'red'} } }, done);
     });
 });
